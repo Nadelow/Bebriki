e031204104597a240712db8895ff7a9bb41591ad
Nozdormuu <113297594+Nozdormuu@users.noreply.github.com>
2023-04-03 10:54:51 +0300

    Update LexBase.cpp

--- LexBase.cpp
+++ LexBase.cpp
@@ -2,3 +2,15 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include "LexBase.h"
+
+LexBase::~LexBase()
+{
+}
+
+void LexBase::Print()
+{
+    if (!m_lexems.empty())
+        for (int i = 0; i < m_lexems.size() - 1; i++)
+            if (m_lexems[i].lexem_type != "rem_cont")
+                std::cout << m_lexems[i].line << '\t' << m_lexems[i].lexem_type << '\t' << m_lexems[i].lexem_name << std::endl;
+}
