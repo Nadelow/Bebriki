c8aa5365eb34319fa38b01d1851dc81857dd9076
Nozdormuu <113297594+Nozdormuu@users.noreply.github.com>
2023-04-03 10:50:46 +0300

    Update Translator.cpp

--- Translator.cpp
+++ Translator.cpp
@@ -2,3 +2,22 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include "Translator.h"
+
+void Translator::Translate(std::string filename)
+{
+}
+
+std::vector<Token> Translator::GetLexems(std::string filename, bool toPrint)
+{
+    m_lexems = m_lexer.Run(filename);
+
+    if (toPrint)
+        m_lexer.Print();
+
+    return m_lexems;
+}
+
+bool Translator::CheckSyntax(std::string filename)
+{
+    return false;
+}
