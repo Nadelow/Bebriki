94905b16a0706ddcfa07e5c3cd0f5a9b453f13dc
Nozdormuu <113297594+Nozdormuu@users.noreply.github.com>
2023-04-12 13:43:30 +0300

    Update SyntBase.h

--- SyntBase.h
+++ SyntBase.h
@@ -8,6 +8,11 @@ public:
     virtual ~SyntBase() = 0;
     virtual void Run(std::vector<Token> lexem) = 0;
 protected:
+    /*
+    * @brief Переопределение Run из LexBase.h для избежания ошибок
+    */
+    std::vector<Token> Run(std::string filename);
+protected:
     std::vector<Token> m_lex; // вектор лексем
 };
 
