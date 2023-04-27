3e6742f43c8f1d9e4e6e56e60f3ff4ca6c17ce36
Nozdormuu <113297594+Nozdormuu@users.noreply.github.com>
2023-04-27 06:44:57 +0300

    Update Translator.cpp
    Дополнение к предыдущему коммиту + добавление проверки перед запуском Генератора Кода.

--- Translator.cpp
+++ Translator.cpp
@@ -7,24 +7,35 @@ void Translator::Translate(std::string filename)
 {
     RunLexer(filename);
     CheckSyntax();
-    TranslateCode();
+    
+    if(m_lines[0][0] != "Error")
+        TranslateCode();
 }
 
 std::vector<Token> Translator::RunLexer(std::string filename)
 {
-    m_lexems = m_lexer.Run(filename);
-    m_lexer.Print();
+    if(m_lexer != nullptr)
+        delete m_lexer;
+    m_lexer = new LexicalBlock;
+    m_lexems = m_lexer->Run(filename);
+    m_lexer->Print();
     return m_lexems;
 }
 
 std::vector<std::vector<std::string>> Translator::CheckSyntax()
 {
-    m_lines = m_synt.Run(m_lexems);
+    if(m_synt != nullptr) 
+        delete m_synt;
+    m_synt = new Syntaxer;
+    m_lines = m_synt->Run(m_lexems);
     return m_lines;
 }
 
 void Translator::TranslateCode()
 {
-    m_gen.CodeCreator(m_lines);
+    if(m_gen != nullptr)
+        delete m_gen;
+    m_gen = new CodeGenerator;
+    m_gen->CodeCreator(m_lines);
     std::cout << "Translated code is in out.txt" << std::endl;
 }
